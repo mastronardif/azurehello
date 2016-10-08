1f7e54b73d5596c79f840954a8421786476d89c1
Frank Mastronardi <mastronardif@gmail.com>
2016-10-08 19:11:52 -0400

    added moe sh

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app = express();
 var port = process.env.port || 1337
 
 app.get('/', function (req, res) {
-    res.send('Hello Azure World!');
+    res.send('Hello Azure World!<b>Holy funcking shit it wok.</b>');
 });
 
 var server = app.listen(port, function () {
